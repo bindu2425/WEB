0b32ae8e394050e1bd190001016a83f1efb1af69
bindu2425 <bindu.g135@gmail.com>
2021-04-28 23:04:18 -0500

    Mobile ICP 6

---
+++ mobile icp5/Demo/app/src/main/java/com/example/demo/User.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class User {
+    public String name;
+    public String phone;
+    public User() {
+    }
+
+    public User(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+}
